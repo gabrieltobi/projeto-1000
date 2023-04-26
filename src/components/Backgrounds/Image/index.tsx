6867b496b4619fa5e6eb00ae4279ecaa8366a54a
Gabriel Rocha <gabrieltubiass@gmail.com>
2023-04-25 21:53:31 -0300

    jpg bg to webp

--- src/components/Backgrounds/Image/index.tsx
+++ src/components/Backgrounds/Image/index.tsx
@@ -1,7 +1,7 @@
 export const BackgroundImage = () => {
   return (
     <div className="fixed left-0 top-0 -z-10 h-full w-full overflow-hidden">
-      <div className="h-full w-full bg-[url('/banner-bg.jpg')] bg-cover bg-top brightness-50 contrast-50 grayscale saturate-50" />
+      <div className="h-full w-full bg-[url('/banner-bg.webp')] bg-cover bg-top brightness-50 contrast-50 grayscale saturate-50" />
     </div>
   );
 };
